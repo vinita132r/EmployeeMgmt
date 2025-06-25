b6ccd34f6ad9c3702e12565c464dc47dd5af249f
vinita132r <140709306+vinita132r@users.noreply.github.com>
2025-06-25 10:43:10 +0530

    Add files via upload

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 var express=require("express");
 var usercontroller=require("./Controllers/Usercontroller");
 var likecontroller=require("./Controllers/Likecontroller");
+const dailyupdate = require("./Controllers/Dailyupdatecontroller");
 
 
 var route=express.Router();
@@ -9,12 +10,21 @@ route.post('/reguser',usercontroller.upload2,usercontroller.saveuser);
 route.post('/viewuser/:pid',usercontroller.viewuser);
 route.post('/findalluser',usercontroller.findalluser);
 route.post('/deleteuser/:pid',usercontroller.deleteuser);
-route.post('/updateuser/:pid',usercontroller.updateuser);
+route.post('/updateuser/:pid',usercontroller.upload2,usercontroller.updateuser);
 route.post('/loguser',usercontroller.loginvalidateuser,);
 route.post('/actuser/:pid',usercontroller.actuser,);
 
 route.post('/likeuser',likecontroller.savelikeuser,);
 route.post('/findliker',likecontroller.findliker,);
+
+route.post('/savedailyupdate/:pid',dailyupdate.savedailyupdate,);
+route.post('/getallupdates/',dailyupdate.getallupdates,);
+route.post('/verifyupdate/:pid',dailyupdate.verifyupdate,);
+route.post('/getusernotifications/',dailyupdate.getusernotifications,);
+
+
+
+
 
 
 module.exports=route;
